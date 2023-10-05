f09777b7dc3025818397da640ec54664d37aeff1
Mateus de Souza Sipauba <mateus.sipauba@sodinenet.com.br>
2023-10-05 20:07:59 -0300

    Versão 0.3 O programa já funciona como deveria, mas ainda falta torná-lo mais intuitivo. É necessário acrescentar um comando que dê instruções.

--- altera-filial.py
+++ altera-filial.py
@@ -21,6 +21,90 @@ TOKEN = '6418781436:AAE8hyxEjX-TjZec0ghGQVArNXAv6Vulcaw'
 # Crie uma instância do bot
 bot = telebot.TeleBot(TOKEN)
 
+# Comando /6cd
+@bot.message_handler(commands=['6cd'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 1 where codigo = 6')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 6 configurada como CD.")
+
+# Comando /6loja
+@bot.message_handler(commands=['6loja'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 2 where codigo = 6')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 6 configurada como LOJA.")
+    
+# Comando /3cd
+@bot.message_handler(commands=['3cd'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 1 where codigo = 3')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 3 configurada como CD.")
+    
+# Comando /3loja
+@bot.message_handler(commands=['3loja'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 2 where codigo = 3')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 3 configurada como LOJA.")
+    
+# Comando /4cd
+@bot.message_handler(commands=['4cd'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 1 where codigo = 4')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 4 configurada como CD.")
+    
+# Comando /4loja
+@bot.message_handler(commands=['4loja'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 2 where codigo = 4')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 4 configurada como LOJA.")
+    
+# Comando /5cd
+@bot.message_handler(commands=['5cd'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 1 where codigo = 5')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 5 configurada como CD.")
+    
+# Comando /5loja
+@bot.message_handler(commands=['5loja'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 2 where codigo = 5')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 5 configurada como LOJA.")
+    
+# Comando /20cd
+@bot.message_handler(commands=['20cd'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 1 where codigo = 20')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 20 configurada como CD.")
+    
+# Comando /20loja
+@bot.message_handler(commands=['20loja'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 2 where codigo = 20')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 20 configurada como LOJA.")
+    
+# Comando /70cd
+@bot.message_handler(commands=['70cd'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 1 where codigo = 70')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 70 configurada como CD.")
+    
+# Comando /70loja
+@bot.message_handler(commands=['70loja'])
+def cd6(message):
+    cursor.execute('update pcfilial set tipofilial = 2 where codigo = 70')
+    cursor.execute('commit')
+    bot.send_message(message.chat.id, "Filial 70 configurada como LOJA.")
+
 # Comando /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
